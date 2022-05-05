7174738c6a9547c9c63fa767a389239af4fba80b
sasakiy916 <sasaki3y98521j@gmail.com>
2022-05-05 21:30:13 +0900

    モンスター追加、csvファイルからデータ読み込み

--- sw2_0Java/src/Monster.java
+++ sw2_0Java/src/Monster.java
@@ -1,6 +1,6 @@
+import java.util.List;
 
-
-public abstract class Monster extends Character{
+public class Monster extends Character{
 	private int avoi;//回避力
 	private int pre;//先制力
 	private int hit;//命中力
@@ -10,6 +10,31 @@ public abstract class Monster extends Character{
 	private int money;//落とすお金
 	private int exp;//落とす経験点
 	
+	public Monster() {
+		
+	}
+	public Monster(String monsterName,String path) {
+		setName(monsterName);
+		List<String[]> monsters = Option.load(path);
+		for(int i=1;i<monsters.size();i++) {
+			if(monsterName.equals(monsters.get(i)[0])) {
+				for(int j=0;j<monsters.get(i).length;j++) {
+					if(monsters.get(0)[j].equals("命中力")) setHit(Integer.parseInt(monsters.get(i)[j]));
+					if(monsters.get(0)[j].equals("固定命中力")) setFixedHit(Integer.parseInt(monsters.get(i)[j]));
+					if(monsters.get(0)[j].equals("打撃点")) setAddDamage(Integer.parseInt(monsters.get(i)[j]));
+					if(monsters.get(0)[j].equals("回避力")) setAvoi(Integer.parseInt(monsters.get(i)[j]));
+					if(monsters.get(0)[j].equals("固定回避力")) setFixedAvoi(Integer.parseInt(monsters.get(i)[j]));
+					if(monsters.get(0)[j].equals("防護点")) setDef(Integer.parseInt(monsters.get(i)[j]));
+					if(monsters.get(0)[j].equals("HP")) setHp(Integer.parseInt(monsters.get(i)[j]));
+					if(monsters.get(0)[j].equals("MP")) setMp(Integer.parseInt(monsters.get(i)[j]));
+					if(monsters.get(0)[j].equals("先制値")) setPre(Integer.parseInt(monsters.get(i)[j]));
+					if(monsters.get(0)[j].equals("ガメル")) setMoney(Integer.parseInt(monsters.get(i)[j]));
+					if(monsters.get(0)[j].equals("経験点")) setExp(Integer.parseInt(monsters.get(i)[j]));
+				}
+				break;
+			}
+		}
+	}
 	//先制判定
 	@Override
 	public int judgePre() {
@@ -74,5 +99,10 @@ public abstract class Monster extends Character{
 	}
 	public void setExp(int exp) {
 		this.exp = exp;
+	}
+	@Override
+	public int damageRoll() {
+		// TODO 自動生成されたメソッド・スタブ
+		return 0;
 	}
 }
