2a3d2e9b0c0ceddfa515425f7e9b8b1989873f82
arukreid <sasaki3y98521j@gmail.com>
2022-05-06 14:07:14 +0900

    動作確認用プロジェクト追加

---
+++ sw2_0/src/Race.java
@@ -0,0 +1,16 @@
+
+
+public enum Race {
+	HUMAN("人間"),
+	ELF("エルフ");
+	
+	private final String name;
+	
+	Race(String name){
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
